dbe1da11c4c920341bd959575dd067b67a984c07
Vamsi krishna <135494128+Dreamervamsi@users.noreply.github.com>
2026-06-22 14:16:04 +0000

    add multer for file content extraction

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,21 @@
-import express from 'express';
+import * as fs from 'node:fs';
+import mupdf from 'mupdf';
+import express,{type Request, type Response} from 'express';
+import 'dotenv/config';
+import multer from 'multer';
+
+const storage = multer.memoryStorage(); 
+const upload = multer({ storage: storage });
 
 const app = express();
+
+app.use(express.json({limit:'16kb'}));
+
+app.post('/upload',upload.array('documents',2),(_req:Request,res:Response)=>{
+    const files = _req.files;
+
+    console.log(files);
+});
+
 
 export default app;
