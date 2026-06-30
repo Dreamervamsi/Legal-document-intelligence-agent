8b945c33eb1308a2194b354cb30e530947b7549c
vamsi krishna <kvamsi.nellore@gmail.com>
2026-06-30 16:42:04 +0530

    add route for risk analaysis

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 import express,{type Request, type Response} from 'express';
 import 'dotenv/config';
-import router from './routes/LoadDocument.route.js';
+import documentRouter from './routes/LoadDocument.route.js';
+import riskAnalysisRouter from './routes/RiskAnalysis.route.js';
 
 
 const app = express();
 
 app.use(express.json({limit:'16kb'}));
 
-app.use('/', router);
+app.use('/', documentRouter);
+app.use('/risk', riskAnalysisRouter);
 
 export default app;
 
